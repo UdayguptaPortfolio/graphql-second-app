8ffebeccd76343e07f777438577bcce6fc660c4b
udayongraph <uday.ongraph@gmail.com>
2022-01-31 19:28:18 +0530

    Backend Complete

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express=require('express');
-const {graphqlHTTP}=require('express-graphql');
-const schema=require('../schema/schema')
+const graphqlHTTP=require('express-graphql');
+const schema=require('./schema/schema')
+const mongoose=require('mongoose')
+
 const app=express();
+
+mongoose.connect('mongodb+srv://Udaygupta12:Udaygupta123@cluster0.2nekh.mongodb.net/graphQL_book_details?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('Database Connected');
+});
 
 app.use('/graphql',graphqlHTTP({
     schema,
