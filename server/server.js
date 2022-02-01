d9af954611105f187ff12c47537f90633ddb487f
udayongraph <uday.ongraph@gmail.com>
2022-02-01 18:17:33 +0530

    App Completed

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express=require('express');
 const graphqlHTTP=require('express-graphql');
 const schema=require('./schema/schema')
 const mongoose=require('mongoose')
+const cors=require('cors');
 
 const app=express();
+
+app.use(cors());
 
 mongoose.connect('mongodb+srv://Udaygupta12:Udaygupta123@cluster0.2nekh.mongodb.net/graphQL_book_details?retryWrites=true&w=majority');
 mongoose.connection.once('open', () => {
